6aafbc46cc57f4fc3477acc8a921a62f9fba42d6
Svezhii <137283496+Svezhii@users.noreply.github.com>
2023-12-26 15:37:05 +0300

    ---

---
+++ Assets/Lesson_09/HealthDrainAbility.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class HealthDrainAbility : MonoBehaviour
+{
+    [SerializeField]private Button _abilityButton;
+    [SerializeField]private Player _player;
+
+    private float _abilityDuration = 6;
+    private bool _isAbilityActive = false;
+    private Enemy _enemy;
+
+    private void Start()
+    {
+        _abilityButton.interactable = false;
+        _abilityButton.onClick.AddListener(ButtonClick);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            _enemy = enemy;
+            _abilityButton.interactable = true;
+            _isAbilityActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            _enemy = enemy;
+            _abilityButton.interactable = false;
+            _isAbilityActive = false;
+        }
+    }
+
+    private IEnumerator HealthSuck(Enemy enemy)
+    {
+        var waitForSeconds = new WaitForSecondsRealtime(_abilityDuration);
+
+        while (_isAbilityActive)
+        {
+            enemy.TakeDamage(1);
+            _player.Healing(1);
+
+            yield return waitForSeconds;
+        }
+    }
+
+    private void ButtonClick()
+    {
+        if(_enemy != null)
+        {
+            StartCoroutine(HealthSuck(_enemy));
+        }
+    }
+}
